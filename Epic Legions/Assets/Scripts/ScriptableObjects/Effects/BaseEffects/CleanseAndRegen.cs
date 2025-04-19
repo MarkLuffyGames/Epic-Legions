ce5691f4ad67690fd4d911da7120420f9d74c34b
MarkLuffyGames <marcosluffy97@gmail.com>
2025-04-18 22:41:14 -0300

    Nuevo movimiento creado.
    Elixir de la Calma Eterna: Elimina todos los efectos negativos de los aliados y cura 20 puntos de daño.

---
+++ Epic Legions/Assets/Scripts/ScriptableObjects/Effects/BaseEffects/CleanseAndRegen.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cleanse And Regen", menuName = "Hemera Legions/Card Effects/ Cleanse And Regen")]
+
+public class CleanseAndRegen : CardEffect
+{
+    [SerializeField] private int amount;
+    public override void ActivateEffect(Card caster, Card target)
+    {
+        target.CleanAllNegativeEffects();
+        target.ToHeal(amount);
+    }
+
+    public override void ActivateEffect(Card caster, List<Card> target)
+    {
+        foreach (Card card in target)
+        {
+            card.CleanAllNegativeEffects();
+            card.ToHeal(amount);
+        }
+    }
+    public override void DeactivateEffect(Effect effect)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateEffect(Effect effect)
+    {
+        throw new System.NotImplementedException();
+    }
+}
