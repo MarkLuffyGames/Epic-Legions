1bba05f63b613a3026a2752915f793e646d02ef4
MarkLuffyGames <marcosluffy97@gmail.com>
2025-08-16 21:55:11 -1000

    [Add] Nueva carta.
    Equipo Arma:
    -Escudo del Bastión Inamovible: Onda de Choque: Inflige daño a todos los enemigos en la misma línea y reduce su velocidad en 10 durante un turno.

--- Epic Legions/Assets/Scripts/UI/MainMenu.cs
+++ Epic Legions/Assets/Scripts/UI/MainMenu.cs
@@ -89,7 +89,7 @@ public class MainMenu : MonoBehaviour
     {
         for (int i = 0; i < 20; i++)
         {
-            deckCardIds.Add(CardDatabase.allCards[1080].CardID);
+            deckCardIds.Add(CardDatabase.allCards[1081].CardID);
         }
         for (int i = 0; i < 40; i++)
         {
